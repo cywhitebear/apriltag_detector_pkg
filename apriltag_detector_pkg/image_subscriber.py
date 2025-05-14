c59b23c61c0ba05901af36a8f157744820203827
yun <yun@Tello0513.myguest.virtualbox.org>
2025-05-15 02:24:42 +0800

    Remove camera intrinsics

--- apriltag_detector_pkg/image_subscriber.py
+++ apriltag_detector_pkg/image_subscriber.py
@@ -38,13 +38,13 @@ class ImageSubscriber(Node):
         self.pose_pub = self.create_publisher(PoseStamped, '/drone_pose', 10)
 
         # === Camera intrinsics === (You must adjust based on your Tello's camera)
-        self.fx = 894.9263444006074  # Focal length x
-        self.fy = 893.4894330190366  # Focal length y
-        self.cx = 491.37027575064906  # Principal point x (image center)
-        self.cy = 394.1484784148441  # Principal point y
+        self.fx = ???  # Focal length x
+        self.fy = ???  # Focal length y
+        self.cx = ???  # Principal point x (image center)
+        self.cy = ???  # Principal point y
 
         # Tag size in meters
-        self.tag_size = 0.16
+        self.tag_size = ???
 
         self.get_logger().info("Pose publisher Initialized.")
 
@@ -55,7 +55,6 @@ class ImageSubscriber(Node):
         """
         tag_world_poses = {
             0: ([0.0, 0.0, 1.5], [-np.pi/2, 0.0, np.pi/2]),  # x, y, z
-            1: ([1.0, 0.0, 1.5], [0.0, np.pi/2, 0.0]),
             # Add more as needed
         }
 
